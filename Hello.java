860986cc0e8eb90224b1aad10c26e14e6eeee0c1
shalinipatil123 <shalinipatil292@gmail.com>
2026-05-29 20:04:25 +0530

    next line

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public  static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println(" My World");
         System.out.println("");
         
     }
