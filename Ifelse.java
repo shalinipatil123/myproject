cf5dda5610abad48c28c5a57fdf0c27d1687fd90
shalinipatil123 <shalinipatil292@gmail.com>
2026-06-03 19:24:46 +0530

    added Ifelse.java

---
+++ Ifelse.java
@@ -0,0 +1,13 @@
+public class Ifelse {
+    public static void main(String[] args){
+        int n=8;
+        if(n%2==0){
+            System.out.println("even number");
+        }
+        else{
+            System.out.println("odd number");
+        }
+        }
+    }
+    
+
